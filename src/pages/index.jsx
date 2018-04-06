74f51371656736b29355cf2ddbe5dff0d95fee1b
Michael Ryberg Hartmann <mrh@planday.com>
2018-04-07 00:31:08 +0200

    Small change

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,7 +25,7 @@ class Index extends React.Component {
             </Hero>
           </IndexHeadContainer>
           <BodyContainer>
-            <h2>A Gatsby Template for Content</h2>
+            <h2>A Gatsby Template for Contenter123</h2>
             <p>Made for modern documentation sites. Table of Contents automatically generated from markdown files. </p>
             <CtaButton to={'/lesson-one'}>See Your First Post</CtaButton>
           </BodyContainer>
@@ -64,7 +64,7 @@ export const pageQuery = graphql`
       limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }
     ) {
-      edges { 
+      edges {
         node {
           fields {
             slug
